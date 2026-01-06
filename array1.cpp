3c960284037639d4db495f1e2a00fd880c0f4268
christianarvy <christianarvypasion@gmail.com>
2026-01-06 01:13:02 +0000

    kuya fishball

---
+++ array1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr[6] = {1,2,4,6,7,5};
+
+    for (int i = 0; i < 6; i++) {
+        cout << arr[i] << " ";
+    }
+   return 0; 
+}
+
+ 
+ 
+ 
